b20227dc5cebfa20e2ac00ab7bb5b7c781adcccf
THWU <THWU@users.noreply.github.com>
2019-03-25 16:59:36 +0800

    [Enhance] mssql-scripter/ScriptExport.bat update - add chcp command

--- mssql-scripter/ScriptExport.bat
+++ mssql-scripter/ScriptExport.bat
@@ -1,10 +1,17 @@
-﻿@echo OFF
+﻿REM =========================
+REM Bat File 本身若是 UTF8 語系, 為了讓 Bat 檔裡的中文字正常顯示與讀取
+REM 必須利用 chcp 變更 Command Line 語系為 UTF8 (代碼為 65001)
+REM 否則中文字會是亂碼, 甚至造成 Java Application 執行錯誤
+REM =========================
+chcp 65001
+
+@echo OFF
 
 ::設定自訂變數
-SET /P ServerName=請輸入資料庫伺服器名稱：
-SET /P DatabaseName=請輸入資料庫名稱：
-SET /P user=請輸入登入帳號：
-SET /p password=請輸入登入密碼：
+SET /P ServerName=DB Server Name:
+SET /P DatabaseName=DB Name:
+SET /P user=Account:
+SET /p password=Password:
 
 echo -------------匯出開始-------------
 ::判斷 Tables 資料夾是否建立
@@ -46,5 +53,18 @@ del %CD%\StoredProcedures\*.sql
 )
 echo 匯出 StoredProcedures 結構
 start "Export Stored Procedures schema" /B mssql-scripter -S %ServerName% -d %DatabaseName% -U %user% -P %password% -f %CD%\StoredProcedures\ --file-per-object --script-create --include-types StoredProcedure --exclude-headers
+
+:_CreateUserDefinedFunction
+::判斷 UserDefinedFunctions 資料夾是否建立
+echo 判斷 UserDefinedFunctions 資料夾是否建立
+IF NOT EXIST "%CD%\UserDefinedFunctions\" (
+md UserDefinedFunctions
+echo 建立 UserDefinedFunctions 資料夾完成
+) ELSE (
+echo UserDefinedFunctions 資料夾已存在，刪除裡面所有檔案
+del %CD%\UserDefinedFunctions\*.sql
+)
+echo 匯出 UserDefinedFunctions 結構
+start "Export Stored Procedures schema" /B mssql-scripter -S %ServerName% -d %DatabaseName% -U %user% -P %password% -f %CD%\UserDefinedFunctions\ --file-per-object --script-create --include-types UserDefinedFunction --exclude-headers
 
 echo -------------匯出結束-------------
